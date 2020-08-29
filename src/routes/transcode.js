6006b7ca3732d1af172786392021ba367fe0c76e
Jaucordi <jaucordi@gmail.com>
2020-08-29 12:03:14 +0200

    - Added `translateToSassOptions` to translate the `codemirror` editor's options into `node-sass` transpiler options for futur uses - Implemented the fact you can now pass the sass options to the transpiler

--- src/routes/transcode.js
+++ src/routes/transcode.js
@@ -2,14 +2,29 @@ const express   = require('express');
 const router    = express.Router();
 const transpile = require('../sass.js');
 
+function translateToSassOptions(opts) {
+	const options = {
+		indentedSyntax: true,
+		indentType:     "tab",
+		indentWidth:    1,
+		linefeed:       'crlf',
+		sourceMap:      false,
+		outputStyle:    "compact",
+		precision:      3,
+	};
+	// console.info(opts, options);
+	return options
+}
+
 router.use(express.json());
 router.route('/sass')
       .post((req, res) => {
-	      const json = req.body;
+	      const {options, ...json} = req.body;
+	      const sassOptions        = translateToSassOptions(options);
 	      transpile(json, (response) => {
-		      const json = JSON.stringify(response);
-		      res.json(json);
-	      });
+		      const jsonRes = JSON.stringify(response);
+		      res.json(jsonRes);
+	      }, sassOptions);
       });
 
 module.exports = router;
