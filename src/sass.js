4c7b38f03fb0fb4bf3baf6568663687983600966
Jaucordi <jaucordi@gmail.com>
2020-08-29 12:00:01 +0200

    - Added default SASS options (for futur features) - Added `options` parameter of `transpile` function for node-sass options futur uses - Added the fact you can ask for file or code now with `file` request's property - Fix the error transmission

--- src/sass.js
+++ src/sass.js
@@ -1,30 +1,54 @@
 const Sass = require('node-sass');
 
+const defaultSassOptions = {
+	indentedSyntax: true,
+	indentType:     "tab",
+	indentWidth:    1,
+	linefeed:       'crlf',
+	sourceMap:      false,
+	outputStyle:    "compact",
+	precision:      3,
+};
+
 /**
  *
- * @param { { sass?: string, scss?: string, to: string} } request
- * @param { (result: { started_at: string, css: string }) => void } cb
+ * @param { { file?: string, sass?: string, scss?: string, to: string} } request
+ * @param { (result: { started_at: string, css?: string, error?: string, ended_at: string }) => void } cb
+ * @param { typeof defaultSassOptions | undefined } options
  */
-function transpile(request, cb) {
-	const {sass, scss, to} = request;
-	const resJson          = {
+function transpile(request, cb, options) {
+	const {file, sass, scss} = request;
+	const resJson                = {
 		started_at: new Date().toISOString(),
 	}
 
+	const opts = {
+		...defaultSassOptions,
+		...options,
+	};
+
+	if (!!file)
+		opts.file = file;
+	else
+		opts.data = sass || scss || "";
+
+	opts.indentedSyntax = !!sass;
+
+	if (opts.watch === true){
+		Sass.render(opts, )
+	} else {
+
+	}
+
 	try {
-		const result = Sass.renderSync({
-			                               data:           sass || scss || "",
-			                               indentedSyntax: !!sass,
-		                               });
-		// @var { Buffer } css
-		const { css } = result;
-		resJson.css = css.toString();
+		const resultDisplay  = Sass.renderSync(opts);
+		resJson.css          = resultDisplay.css.toString();
 	} catch (e) {
-		resJson.css = e.message;
+		resJson.error = e.message;
 	} finally {
 		resJson.ended_at = new Date().toISOString();
-		cb(resJson);
 	}
+	cb(resJson);
 }
 
 module.exports = transpile;
